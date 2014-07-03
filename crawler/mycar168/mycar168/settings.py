aa884d552a80265e77a71f91ed407e9d77840d56
yzhuan <yzhuan0621@gmail.com>
2014-07-03 10:49:12 +0800

    new file:   crawler/mycar168/mycar168/__init__.py 	new file:   crawler/mycar168/mycar168/items.py 	new file:   crawler/mycar168/mycar168/pipelines.py 	new file:   crawler/mycar168/mycar168/settings.py 	new file:   crawler/mycar168/mycar168/spiders/__init__.py 	new file:   crawler/mycar168/scrapy.cfg

---
+++ crawler/mycar168/mycar168/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for mycar168 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'mycar168'
+
+SPIDER_MODULES = ['mycar168.spiders']
+NEWSPIDER_MODULE = 'mycar168.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'mycar168 (+http://www.yourdomain.com)'
